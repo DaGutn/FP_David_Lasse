2166e110216deecfc7b4e8b886980d36a8f9189c
Lasse Sternemann <lasse.sternemann@tu-dortmund.de>
2021-08-03 14:41:26 +0200

    sdf

---
+++ V23_Quanten_Analogien/programm.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+from scipy.optimize import curve_fit
+import scipy.constants as c
+
+###########################################################################################################
+### 
